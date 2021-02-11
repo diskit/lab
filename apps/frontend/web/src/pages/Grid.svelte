85c44c186ad446e7eb06607e405662993045bb8f
diskit <diskit.1980@gmail.com>
2021-02-11 17:20:53 +0900

    add grid

--- apps/frontend/web/src/pages/Grid.svelte
+++ apps/frontend/web/src/pages/Grid.svelte
@@ -8,19 +8,54 @@ onMount(() => {
 </script>
   
 <style lang="scss">
+
+  .content {
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+  }
+
   .container {
+    flex-grow: 1;
     display: grid;
-    grid-template-rows: 100px;
-    grid-template-columns: 100px 1fr;
+    transition: 2s;
+    grid-template-rows: 64px auto;
+    grid-template-columns: 100px auto 280px;
+    grid-template-areas: 
+      "header header header"
+      "menu main-content sub-content";
+  }
+  
+  .header {
+    background: #000;
+    color: #fff;
+    grid-area: header;
+  }
+
+  .menu {
+    background: #333;
+    color: #ddd;
+    grid-area: menu;
+  }
+
+  .main-content {
+    grid-area: main-content;
+  }
+
+  .sub-content {
+    grid-area: sub-content;
+    background: #333;
+    color: #ddd;
   }
 </style>
-  
-
-<h1>Grid</h1>
-<div class="container">
-  {#each Array(10) as _, i}
-    <div>{i+1}</div>
-  {/each}
+<div class="content">
+  <h1>Grid</h1>
+  <div class="container">
+    <div class="header">header</div>
+    <div class="menu">menu</div>
+    <div class="main-content">main-content</div>
+    <div class="sub-content">sub-content</div>
+  </div>
 </div>
 
   
